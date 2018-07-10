ede95d5d89c4b05b96b0e885aea2c1a98ae14a7d
leminhitalia <leminhitalia@gmail.com>
2018-07-10 21:42:00 +0700

    image processing

--- facerec_image_processing/facerec_recognizer.py
+++ facerec_image_processing/facerec_recognizer.py
@@ -64,7 +64,7 @@ while True:
         is_found_user = False
         for user in user_data:
             user_id = str(user['id'])
-            print("[DEBUG] user_id = " + user_id + ", face_id = " + face_id + ", face_id equals user_id = " + str(face_id == user_id))
+            print("[DEBUG] user_id = {}, face_id = {}, face_id equals user_id = {}".format(user_id, face_id, face_id == user_id))
 
             if face_id == user_id:
                 face_id = user['name']
