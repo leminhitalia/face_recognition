cbe5b11376748479bbed548daea4068723940d77
leminhitalia <leminhitalia@gmail.com>
2018-08-04 14:52:09 +0700

    test hash string

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import datetime
 
-print("DEBBUG: date_time = " + str(datetime.datetime.now()).replace(" ","_"))
+# print("DEBBUG: date_time = " + str(datetime.datetime.now()).replace(" ","_"))
+
+import hashlib
+s = 'Y Minh Le'
+# int(hashlib.sha1(s).hexdigest(), 16) % (10 ** 8)
+id = int(hashlib.sha256(s.encode('utf-8')).hexdigest(), 16) % (10**8)
+print("id = {}".format(id))
